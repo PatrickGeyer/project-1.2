5e5d6fcca6106ac08fe164975864edbf17e9996d
Patrick Geyer <patrick@evolut.net>
2020-03-16 11:45:13 +0100

    =Example Commit

--- src/main/java/golf/physics/Function2d.java
+++ src/main/java/golf/physics/Function2d.java
@@ -4,3 +4,20 @@ interface Function2d {
    public double evaluate(Vector2d p);
    public Vector2d gradient(Vector2d p);
 }
+
+public class Function2d {
+   public String function;
+   public Function2d(String function) {
+      this.function = function;
+   }
+   public double evaluate(Vector2d p) {
+      return new ExpressionBuilder(this.function)
+            .variables("x", "y")
+            .build()
+            .setVariable("x", x)
+            .setVariable("y", y).evaluate();
+   }
+   public Vector2d gradient(Vector2d p) {
+
+   }
+}
