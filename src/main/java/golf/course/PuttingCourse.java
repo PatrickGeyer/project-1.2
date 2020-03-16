b7f42a703e5be415127e1be73e8622dd33ea0b17
quietK <bmsbuilder@gmail.com>
2020-03-16 17:15:58 +0100

    PuttingCourse class created

--- src/main/java/golf/course/PuttingCourse.java
+++ src/main/java/golf/course/PuttingCourse.java
@@ -3,36 +3,62 @@ import golf.physics.*;
 import net.objecthunter.exp4j.*;
 
 public class PuttingCourse {
-    public Function2d height;
-    public Vector2d flag = new Vector2d(10, 10);
-    public Vector2d start = new Vector2d(0,0);
+    private Function2d height;
+    private Vector2d flag = new Vector2d(0, 10);
+    private Vector2d start = new Vector2d(0,0);
+    private double g = 9.81;              // Gravitational acceleration
+    private double frictionCoefficient = 0.131;            // Coefficient of friction (rolling ball) // Typical 0.065<=mu<=0.196
+    private double Vmax = 3.0;            // Maximum initial ball speed [m/s]
+    private double holeTolerance = 0.02;  // Distance from hole for a successful putt [m]
 
-
-    public double g = 9.81;              // Gravitational acceleration
-    public double ballMass = 45.00;    // Mass of ball (g)
-    public double frictionCoefficient = 0.131;            // Coefficient of friction (rolling ball) // Typical 0.065<=mu<=0.196
-    public double vmax = 3.0;            // Maximum initial ball speed [m/s]
-    public double goalTolerance = 0.02;  // Distance from hole for a successful putt [m]
-    public String height = "0";         // String of a function that returns height for each x, y coordinate
-
-    public double getHeight(double x, double y) {
-        Expression e = new ExpressionBuilder(this.height)
-            .variables("x", "y")
-            .build()
-            .setVariable("x", x)
-            .setVariable("y", y);
-        return e.evaluate();
+    public PuttingCourse (Function2d height, Vector2d flag, Vector2d start) {
+        this.height = height;
+        this.flag = flag;
+        this.start = start;
     }
 
-    public double friction(Vector2d pos) {
-        return 0.0;
+    public Function2d get_height() {
+        return this.height;
     }
-    // public PuttingCourse(Function2d height,
-    //                      Vector2d flag, Vector2d start);
-    // public Function2d get_height();
-    // public Vector2d get_flag_position(); 
-    // public Vector2d get_start_position(); 
-    // public double get_friction_coefficient(); 
-    // public double get_maximum_velocity(); 
-    // public double get_hole_tolerance();
+
+    public Vector2d get_flag_position() {
+        return this.flag;
+    }
+
+    public Vector2d get_start_position() {
+        return this.start;
+    }
+
+    public double get_friction_coefficient() {
+        return frictionCoefficient;
+    }
+
+    public double get_maximum_velocity() {
+        return Vmax;
+    }
+
+    public double get_hole_tolerance() {
+        return holeTolerance;
+    }
+
+    public double get_gravitational_constant() {
+        return g;
+    }
+
+    public void setFrictionCoefficient(double friction) {
+        this.frictionCoefficient = friction;
+    }
+
+    public void setMaximumVelocity(double Vmax) {
+        this.Vmax = Vmax;
+    }
+
+    public void setHoleTolerance(double tolerance) {
+        this.holeTolerance = tolerance;
+    }
+
+    public void setGravitationalConstant(double g) {
+        this.g = g;
+    }
+
 }
