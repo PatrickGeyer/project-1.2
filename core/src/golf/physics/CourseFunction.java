321bab27a79916dcb8aa6cde2f63cf5b994d98fb
quietK <bmsbuilder@gmail.com>
2020-03-19 19:27:46 +0100

    ~

---
+++ core/src/golf/physics/CourseFunction.java
@@ -0,0 +1,39 @@
+package golf.physics;
+import golf.physics.*;
+import net.objecthunter.exp4j.*;
+
+public class CourseFunction implements Function2d {
+   public String function;
+   
+   public Function2d(String function) {
+      this.function = function;
+   }
+
+   public double evaluate(Vector2d p) {
+      return new ExpressionBuilder(this.function)
+            .variables("x", "y")
+            .build()
+            .setVariable("x", p.get_x())
+            .setVariable("y", p.get_y()).evaluate();
+   }
+
+   public double evaluate(double x, double y) {
+      return evaluate(new Vector2d(x, y));
+   }
+
+   public Vector2d gradient(Vector2d p) {
+      double z = evaluate(p);
+      double pdx = evaluate(new Vector2d(p.get_x()+DELTA, p.get_y()));
+      double pdy = evaluate(new Vector2d(p.get_x(), p.get_y()+DELTA));
+      return new Vector2d((pdx-z)/DELTA, (pdy-z)/DELTA);
+   }
+
+   public double partialDerivativeX(double x, double y) {
+      return 0.0;
+   }
+   
+   public double partialDerivativeY(double x, double y) {
+      return 0.0;
+   }
+
+}
--- core/src/golf/physics/Function2d.java
+++ core/src/golf/physics/Function2d.java
@@ -1,36 +1,8 @@
 package golf.physics;
+import golf.physics.*;
 
-import net.objecthunter.exp4j.*;
-
-// interface Function2d {
-//    public double evaluate(Vector2d p);
-//    public Vector2d gradient(Vector2d p);
-// }
-
-public class Function2d {
-   public String function;
-   public Function2d(String function) {
-      this.function = function;
-   }
-   public double evaluate(Vector2d p) {
-      return new ExpressionBuilder(this.function)
-            .variables("x", "y")
-            .build()
-            .setVariable("x", p.get_x())
-            .setVariable("y", p.get_y()).evaluate();
-   }
-   public double evaluate(double x, double y) {
-      return evaluate(new Vector2d(x, y));
-   }
-   public Vector2d gradient(Vector2d p) {
-      return new Vector2d(evaluate(p) - evaluate(p.addX(0.01)), evaluate(p) - evaluate(p.addY(0.01)));
-   }
-
-   public double partialDerivativeX(double x, double y) {
-      return 0.0;
-   }
-   public double partialDerivativeY(double x, double y) {
-      return 0.0;
-   }
-
+interface Function2d {
+	double DELTA = 0.00001;
+	public double evaluate(Vector2d p);
+	public Vector2d gradient(Vector2d p);
 }
