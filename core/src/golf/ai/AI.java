312a784cb8fb12d10d9b3e040822474f5c9ad24b
isabel3004 <isabel.d.stein@gmail.com>
2020-04-17 16:59:04 +0200

    Create AI.java

---
+++ core/src/golf/ai/AI.java
@@ -0,0 +1,15 @@
+
+/*package golf.course;
+import golf.physics.*;
+import golf.course.object.*;
+*/
+public class AI {
+     /* public boolean calculate_shot(course, ball){
+                //calculate the direction & strength needed
+                x=
+                y=
+                return Vector2d(x, y);  //returns the direction and strength needed for the shot
+                                        //then proceed with the same actions you would have when playing golf manually
+        }
+*/
+}
