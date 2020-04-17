6dff4bfc5edb82ee0d6f2abff714ec09bc1fdbaf
isabel3004 <isabel.d.stein@gmail.com>
2020-04-17 16:53:18 +0200

    a

---
+++ src/main/java/golf/course-designer/product.java
@@ -0,0 +1,8 @@
+public interface Item {
+    String getFormula();
+    double getFriction();
+    String getStart();
+    String getTarget();
+    double getRadius();
+    double getMaxVel();
+}           
