ca461c13ba1eece18b46fd997d3d456616489730
alicekck <alicek2000@gmail.com>
2020-05-15 02:52:42 +0200

    added correct RungeKutta

--- core/src/golf/physics/RungeKutta.java
+++ core/src/golf/physics/RungeKutta.java
@@ -29,43 +29,91 @@ public class RungeKutta implements PhysicsEngine {
     public Vector3 getAcceleration(Vector3 position, Vector3 velocity, PuttingCourse c) {
         
         Vector2d gradient = c.height.gradient(position.x, position.y);
+        //System.out.println(gradient + " = gradient (RK32));
 
         Vector3 acceleration = new Vector3();
-        acceleration.x = (float) ((-c.g * gradient.x) - c.frictionCoefficient * c.g * velocity.x / (Math.sqrt(velocity.x*velocity.x + velocity.y*velocity.y)));
-        acceleration.y = (float) ((-c.g * gradient.y) - c.frictionCoefficient * c.g * velocity.x / (Math.sqrt(velocity.x*velocity.x + velocity.y*velocity.y)));
-        
-        return acceleration;
+
+        if (velocity.x != 0 || velocity.y != 0) {
+            acceleration.x = (float) ((-c.g * gradient.x) - c.frictionCoefficient * c.g * velocity.x / (Math.sqrt(velocity.x*velocity.x + velocity.y*velocity.y)));
+            acceleration.y = (float) ((-c.g * gradient.y) - c.frictionCoefficient * c.g * velocity.y / (Math.sqrt(velocity.x*velocity.x + velocity.y*velocity.y)));
+            //System.out.println(acceleration.x + " = acceleration.x (RK39)");
+            //System.out.println((velocity.x) + " = velocity.x check (Rk:40)");
+            return acceleration;
+        } 
+        return new Vector3(0,0,0);
     }
 
     @Override
     public Vector3[] solve(GameObject obj, PuttingCourse c, double h) {
-        Vector3 k1v, k1x, k2v, k2x, k3v, k3x, k4v, k4x;
-
-        float s = (float) h;
-       
-        k1v = getAcceleration(obj.position, obj.velocity, c).scl(s);
-        k1x = obj.velocity.scl(s);
-
-        k2v = getAcceleration(obj.position.add(k1x.scl(0.5f)), obj.velocity.add(k1v.scl(0.5f)), c).scl(s);
-        k2x = (obj.velocity.add(k1v.scl(0.5f)).scl(s));
-
-        k3v = getAcceleration(obj.position.add((k1x.scl(-0.5f)).add(k2x.scl(0.5f))), obj.velocity.add((k1v.scl(-0.5f)).add((k2v.scl(0.5f)))), c).scl(s);
-        k3x = (obj.velocity.add((k1v.scl(-0.5f)).add((k2v.scl(0.5f))).scl(s)));
-
-        k4v = getAcceleration(obj.position.add((k2x.scl(-0.5f)).add(k3x)), obj.velocity.add((k2v.scl(-0.5f)).add(k3v)), c).scl(s);
-        k4x = (obj.velocity.add((k2v.scl(-0.5f)).add(k3x))).scl(s);
+        //System.out.println(obj.velocity.x + " = obj.velocity.x (RK48)");
+        //System.out.println(obj.velocity.y + " = obj.velocity.y (RK49)");
 
         Vector3[] response = new Vector3[3];
+        
+        double k1x = obj.velocity.x * h;
+        double k2x = (obj.velocity.x + k1x/2) * h;
+        double k3x = (obj.velocity.x + k2x/2) * h;
+        double k4x = (obj.velocity.x + k3x) * h;
 
-        // Position vector
-        response[0] = (k1x.add((k2x.scl(2))).add((k3x.scl(2))).add(k4x)).scl(1/6);
+        float updX = (float) ((k1x + 2*k2x + 2*k3x + k4x) / 6);
 
-        // Velocity vector
+        double k1y = obj.velocity.y * h;
+        double k2y = (obj.velocity.y + (k1y/2)) * h;
+        double k3y = (obj.velocity.y + (k2y/2)) * h;
+        double k4y = (obj.velocity.y + k3y) * h;
+        
+        float updY = (float) ((k1y + 2*k2y + 2*k3y + k4y) / 6);
+        
+        // Calculte the change for z (now - before)
+        float updZ = (float) (c.height.evaluate(obj.position.x + updX, obj.position.y + updY)) - (float) (c.height.evaluate(obj.position.x, obj.position.y));
+        response[0] = new Vector3(updX, updY, 0);
+        response[0] = response[0].add(obj.position);
+        //response[0].z = (float) c.height.evaluate(obj.position.x + updX, obj.position.y + updY);
+        response[0].z = (float) c.height.evaluate(response[0].x, response[0].y);
+
+        //////////System.out.println(response[0].z + " = response[0].z (RK74)");
+        //////////System.out.println(Math.sin(response[0].x + " = sin check (RK75)");
+
+        double k1vx = getAcceleration(obj.velocity, obj.position, c).x * h;
+        double k2vx = getAcceleration(obj.velocity.add((float) (k1vx/2)), obj.position.add( (float) (k1x/2)), c).x * h;
+        obj.velocity.add((float) (-k1vx/2));
+        obj.position.add((float) (-k1x/2));
+        
+        double k3vx = getAcceleration(obj.velocity.add((float) (k2vx/2)), obj.position.add((float) (k2x/2)), c).x * h;
+        obj.velocity.add((float) (-k2vx/2));
+        obj.position.add((float) (-k2x/2));
+        
+        double k4vx = getAcceleration(obj.velocity.add((float) (k3vx)),obj.position.add((float) (k3x)), c).x * h;
+        obj.velocity.add((float) (-k3vx)); 
+        obj.position.add((float) (-k3x));
+
+        float updXV = (float) (k1vx + 2*k2vx + 2*k3vx + k4vx)/6;
+
+        double k1vy = getAcceleration(obj.velocity,obj.position, c).y * h;
+        double k2vy = getAcceleration(obj.velocity.add((float) (k1vy/2)), obj.position.add((float) (k1y/2)), c).y * h;
+        obj.velocity.add((float) (-k1vy/2));
+        obj.position.add((float) (-k1y/2));
+        
+        double k3vy = getAcceleration(obj.velocity.add((float) (k2vy/2)), obj.position.add((float) (k2y/2)), c).y * h;
+        obj.velocity.add((float) (-k2vy/2));
+        obj.position.add((float) (-k2y/2));
+        
+        double k4vy = getAcceleration(obj.velocity.add((float) (k3vy)), obj.position.add((float) (k3y)), c).y * h;
+        obj.velocity.add((float) (-k3vy));
+        obj.position.add((float) (-k3y));
+        
+        float updYV = (float) (k1vy + 2*k2vy + 2*k3vy + k4vy)/6;
+
+        response[1] = new Vector3(updXV, updYV, 0f);
+        response[1] = response[1].add(obj.velocity);
+
         Vector3 f = this.force(obj, c, h);
-
-        response[1] = (k1v.add((k2v.scl(2))).add((k3v.scl(2))).add(k4v)).scl(1/6);
-
         response[2] = f;
+        
+        //System.out.println((obj.velocity.x) + " (124check)");
+        //System.out.println(obj.position.z + "xx");
+        //System.out.println(response[0].z + "xx");
+
         return response;
         
     }
