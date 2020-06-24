1ca7d8e367e2b65a96bffbd82ba00d26576ab6b7
KarlyB <k.bradley@student.maastrichtuniversity.nl>
2020-06-24 10:32:50 +0200

    Update Sandpit.java

--- core/src/golf/course/object/Sandpit.java
+++ core/src/golf/course/object/Sandpit.java
@@ -2,5 +2,15 @@ package golf.course.object;
 import golf.course.object.Obstacle;
 
 public class Sandpit extends Obstacle {
+  
+  public boolean isSand (double x, double y){
+
+        if (x>10 && x<17 && y>19 && y<28){ //dimension of sandpit
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
